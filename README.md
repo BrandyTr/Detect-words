7d643ab00e94c1b28580f168121615665f43b28f
qthais <thaiallb63@gmail.com>
2025-05-24 09:39:19 +0700

    feat(save words)

--- README.md
+++
@@ -1,8 +0,0 @@
-# Detect-words
-
-### How to run this code
-**Step 1:** Open terminal and run this code `python run.py gen`<br>
-**Step 2:** After running the code above, the program will ask you to enter the words needed to detect. Enter the words.<br>
-**Step 3:** Enter `python run.py test` to see the result<br>
-
-**Note**: This program tests the text file `(.txt)` in the `tests` folder, so remember to enter the sentences/words/information you need to test in those files.
--- detect-words/src/components/WordDetector.tsx
+++ detect-words/src/components/WordDetector.tsx
@@ -1,14 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Button from "./Button"
-
 export default function WordDetector() {
     const [word, setWord] = useState('')
-
-    return(
+    const [words, setWords] = useState<string[]>([])
+    const handleSave = async (word: string) => {
+        if (!words.includes(word)) {
+            const updatedWords = [...words, word];
+            setWords(updatedWords);
+            console.log({ updatedWords });
+            localStorage.setItem('savedWords', JSON.stringify(updatedWords))
+        } else {
+            console.log("Duplicated!")
+        }
+    }
+    useEffect(() => {
+        const saved = localStorage.getItem('savedWords');
+        if (saved) {
+            const parsed = JSON.parse(saved);
+            setWords(parsed);
+            setWord('')
+            console.log({initialWords:parsed});
+        }
+    }, [])
+    return (
         <>
             <div className="flex flex-col gap-[6px] mb-[18px]">
                 <label className="relative flex items-center gap-1 text-[14px] text-black font-medium align-center">
-                    Enter the words 
+                    Enter the words
                     <span className="relative group cursor-pointer bg-primary-lightestBlue font-bold text-[8px] text-primary-darkBlue px-[5px] rounded-[100px] border-[1.25px] border-primary-darkBlue">
                         i
 
@@ -22,7 +40,7 @@ export default function WordDetector() {
                 </label>
 
                 <div className="w-full flex overflow-hidden">
-                    <input 
+                    <input
                         type="text"
                         name=""
                         id=""
@@ -38,7 +56,7 @@ export default function WordDetector() {
                 </div>
             </div>
             <div className="flex flex-wrap gap-1">
-                <Button colorClass="bg-gradient-green" name="Save"></Button>
+                <Button onClick={() => handleSave(word)} colorClass="bg-gradient-green" name="Save"></Button>
                 <Button colorClass="bg-gradient-red" name="Hide comments"></Button>
                 <Button colorClass="bg-gradient-orange" name="Generate"></Button>
                 <Button colorClass="bg-gradient-grey" name="List"></Button>
