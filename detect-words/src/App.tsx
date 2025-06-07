3ab7b78f5f8c53838adbb0409e71a6db45832c17
Brandy Tran <baotutran1103@gmail.com>
2025-06-07 20:01:27 +0700

    add icon

--- detect-words/src/App.tsx
+++ detect-words/src/App.tsx
@@ -10,7 +10,7 @@ import WordDetector from './components/WordDetector'
 function App() {
 
   return (
-    <div className='bg-gradient-blue max-w-[700px] min-w-[450px] min-h-[255px]'>
+    <div className='bg-gradient-blue max-w-[700px] min-w-[450px] min-h-[280px]'>
       <Header></Header>
       <main className='pt-16 px-5 font-poppins'>
         <WordDetector></WordDetector>
